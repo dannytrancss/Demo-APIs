ec04b32d0c47f871720db86ba4baf1e986161c3e
dannyvop <thanhsuol@gmail.com>
2021-09-13 02:32:39 +0800

    Create GetAllAgentsParameter.cs
    Adding file

---
+++ Demo.Application/Parameters/Agent/GetAllAgentsParameter.cs
@@ -0,0 +1,12 @@
+using System.Collections.Generic;
+using MediatR;
+using Demo.Application.Parameters;
+using Demo.Domain.Entities;
+
+namespace Demo.Application.Parameters.Agent
+{
+    public class GetAllAgentsParameter : RequestParameter
+    {
+       
+    }
+}
