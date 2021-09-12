3b482340989c9c358da2def9fc068ad72fc36ab8
dannyvop <thanhsuol@gmail.com>
2021-09-13 02:33:16 +0800

    Create AddAgentHandler.cs
    Adding file

---
+++ Demo.Application/Handlers/Agent/AddAgentHandler.cs
@@ -0,0 +1,40 @@
+using AutoMapper;
+using Demo.Application.Commands.Agent;
+using Demo.Application.Parameters;
+using Demo.Application.ViewModels.Agent;
+using Demo.Domain.Entities;
+using Demo.Domain.Interfaces;
+using MediatR;
+using System;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace Demo.Application.Handlers.Agent
+{
+    public class AddAgentHandler : IRequestHandler<AddAgentCommand, Response<AgentViewModel>>
+    {
+        private readonly IAgentRepository _repository;
+        private readonly IMapper _mapper;
+
+        public AddAgentHandler(IAgentRepository repository, IMapper mapper)
+        {
+            _repository = repository;
+            _mapper = mapper;
+        }
+
+        public async Task<Response<AgentViewModel>> Handle(AddAgentCommand request, CancellationToken cancellationToken)
+        {
+            try
+            {
+                var item = _mapper.Map<AgentEntity>(request);
+                await _repository.AddAgentAsync(item);
+
+                return new Response<AgentViewModel>(_mapper.Map<AgentViewModel>(item));
+            }
+           catch(Exception ex)
+            {
+                return new Response<AgentViewModel>(ex.Message);
+            }
+        }
+    }
+}
