b58d5d6baa8a7153894f7d6a83747979ce8c10b2
dannyvop <thanhsuol@gmail.com>
2021-09-13 02:32:57 +0800

    Create GetAllAgentsHandler.cs
    Adding file

---
+++ Demo.Application/Handlers/Agent/GetAllAgentsHandler.cs
@@ -0,0 +1,42 @@
+using AutoMapper;
+using Demo.Application.Parameters;
+using Demo.Application.Parameters.Agent;
+using Demo.Application.Queries.Agent;
+using Demo.Application.ViewModels.Agent;
+using Demo.Domain.Interfaces;
+using MediatR;
+using System;
+using System.Collections.Generic;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace Demo.Application.Handlers.Agent
+{
+    public class GetAllAgentsHandler : IRequestHandler<GetAllAgentsQuery, PagedResponse<IEnumerable<AgentViewModel>>>
+    {
+        private readonly IAgentRepository _repository;
+        private readonly IMapper _mapper;
+
+        public GetAllAgentsHandler(IAgentRepository repository, IMapper mapper)
+        {
+            _repository = repository;
+            _mapper = mapper;
+        }
+
+        public async Task<PagedResponse<IEnumerable<AgentViewModel>>> Handle(GetAllAgentsQuery request, CancellationToken cancellationToken)
+        {
+            try
+            {
+                var validFilter = _mapper.Map<GetAllAgentsParameter>(request);
+                var agentList = await _repository.GetAllAgentsAsync(request.PageNumber, request.PageSize);
+                var agentViewModel = _mapper.Map<IEnumerable<AgentViewModel>>(agentList);
+                return new PagedResponse<IEnumerable<AgentViewModel>>(agentViewModel, validFilter.PageNumber, validFilter.PageSize);
+            }
+            catch(Exception ex)
+            {
+                return new PagedResponse<IEnumerable<AgentViewModel>>(null, request.PageNumber, request.PageSize, false, ex.Message);
+            }
+        }
+    }
+
+}
