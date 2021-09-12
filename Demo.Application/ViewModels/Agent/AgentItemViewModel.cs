2019471b6e408a4d0bc51c81c3c1c26cbda0a5e5
dannyvop <thanhsuol@gmail.com>
2021-09-13 02:31:54 +0800

    Create AgentItemViewModel.cs
    Adding file

---
+++ Demo.Application/ViewModels/Agent/AgentItemViewModel.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Demo.Application.ViewModels.Agent
+{
+    public class AgentViewModel
+    {
+        public Guid Id { get; set; }
+        public string Name { get; set; }
+        public string Email { get; set; }
+        public int TotalProjectsDelivered { get; set; }
+        public int Reviews { get; set; }
+        public DateTimeOffset JoinedDate { get; set; }
+    }
+}
