b528f7411cc660e783e5176fbe732580f5b7b13a
dannyvop <thanhsuol@gmail.com>
2021-09-13 02:32:26 +0800

    Create RequestParameter.cs
    Adding file

---
+++ Demo.Application/Parameters/RequestParameter.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Demo.Application.Parameters
+{
+    public class RequestParameter
+    {
+        const int PAGE_NUMBER_DEFAULT = 1;
+        const int PAGE_SIZE_dEFAULT = 10;
+        public int PageNumber { get; set; }
+        public int PageSize { get; set; }
+        public RequestParameter()
+        {
+            PageNumber = PAGE_NUMBER_DEFAULT;
+            PageSize = PAGE_SIZE_dEFAULT;
+        }
+        public RequestParameter(int pageNumber, int pageSize)
+        {
+            PageNumber = pageNumber < 1 ? PAGE_NUMBER_DEFAULT : pageNumber;
+            PageSize = pageSize > PAGE_SIZE_dEFAULT ? PAGE_SIZE_dEFAULT : pageSize;
+        }
+    }
+}
